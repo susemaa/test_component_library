4046163846c4d9dbb63fd78477db78412120d1b5
Arseny Radoslavov <rad.archeg@gmail.com>
2023-11-19 21:04:43 +0700

    update

--- src/components/EditableTextInput/EditableTextInput.tsx
+++ src/components/EditableTextInput/EditableTextInput.tsx
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
-import { Editor, EditorState, RichUtils } from 'draft-js';
+import { Editor, EditorState, RichUtils, convertFromRaw, convertToRaw, ContentBlock } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 
-const EditableTextInput: React.FC = () => {
-  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+interface EditableTextInputProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const EditableTextInput: React.FC<EditableTextInputProps> = ({ value, onChange }) => {
+  const [editorState, setEditorState] = useState(
+    value ? EditorState.createWithContent(convertFromRaw(JSON.parse(value))) : EditorState.createEmpty()
+    );
 
   const applyStyle = (style: string) => {
     setEditorState(RichUtils.toggleInlineStyle(editorState, style));
@@ -14,10 +21,16 @@ const EditableTextInput: React.FC = () => {
     return currentStyle.has(style);
   };
 
+  const handleChange = (state: EditorState) => {
+    setEditorState(state);
+    onChange(JSON.stringify(convertToRaw(editorState.getCurrentContent())));
+  };
+
   return (
     <div className="editor-container p-4 bg-white shadow-lg rounded">
       <div className="toolbar mb-2">
-        <button 
+        <button
+          type="button"
           className={`px-2 py-1 m-1 text-dark text-bold rounded ${isStyleActive('BOLD') ? 'bg-blue-500' : 'bg-blue-300'}`}
           onMouseDown={(e) => {
             e.preventDefault();
@@ -27,7 +40,7 @@ const EditableTextInput: React.FC = () => {
         </button>
       </div>
       <div className="border border-gray-300 rounded p-2 max-h-96 overflow-y-auto">
-        <Editor editorState={editorState} onChange={setEditorState} />
+        <Editor editorState={editorState} onChange={handleChange} />
       </div>
     </div>
   );
